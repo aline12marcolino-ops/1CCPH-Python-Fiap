5eca723ef9d5eaa7c0a8123e0aaf245b06a9f992
labsfiap <aline12marcolino@gmail.com>
2026-03-17 09:48:23 -0300

    Aula 2- variaveis

--- Aula02/Exercício.py
+++ Aula02/Exercício.py
@@ -8,4 +8,5 @@ Ano= int(input("Digite o ano que você nasceu: "))
 print(Dia,Mês,Ano)
 
 numero=int(input("Digite um número:"))
-numero2=int(input("Digite outro numero"))
+numero2=int(input("Digite outro numero: "))
+Resultado =numero+numero2

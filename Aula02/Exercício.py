6a997dcae136b38e36a83ec99e93b342371839c1
labsfiap <aline12marcolino@gmail.com>
2026-03-17 09:43:21 -0300

    Aula 2- variaveis

---
+++ Aula02/Exercício.py
@@ -0,0 +1,11 @@
+nome=input("Digite seu nome")
+print(f"Bem vino,{nome}")
+
+Dia=int(input("Digite o dia que voce nasceu: "))
+Mês= int(input("Digite o mês que voce nasceu: "))
+Ano= int(input("Digite o ano que você nasceu: "))
+
+print(Dia,Mês,Ano)
+
+numero=int(input("Digite um número:"))
+numero2=int(input("Digite outro numero"))
---
+++ Aula02/aula02A.py
@@ -0,0 +1,21 @@
+print("ola mundo")
+
+print(6+1)
+print("6+1")
+print("7"+"4")
+
+#VARIÁVEIS
+nome="Aline"
+idade=19
+peso=48.0
+
+print(nome,idade,peso)
+
+#input-simula forms no CMD
+nome=input("Digite seu nome: ")
+idade=int(input("Digite sua idade: "))
+peso=float(input("Digite seu peso:  "))
+
+print(nome,idade,peso)
+
+print(f"oii, {nome}!")
